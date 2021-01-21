f84dcafa8cdc0c69413a3801e70e30c14ff01e9e
uyjulian <uyjulian@gmail.com>
2021-01-21 11:12:22 -0600

    Use my fork of ps2toolchain

--- scripts/001-toolchain.sh
+++ scripts/001-toolchain.sh
@@ -2,11 +2,11 @@
 # ps2-packer.sh by fjtrujy
 
 ## Download the source code.
-REPO_URL="https://github.com/ps2dev/ps2toolchain"
+REPO_URL="https://github.com/uyjulian/ps2toolchain"
 REPO_FOLDER="ps2toolchain"
 
 # Checking if a specific TAG has been selected, it is passed using parameter $1
-[  -z "$1" ] && REPO_REFERENCE="origin/master" || REPO_REFERENCE=$1
+[  -z "$1" ] && REPO_REFERENCE="origin/uyjworking" || REPO_REFERENCE=$1
 echo "Using repo reference $REPO_REFERENCE"
 
 if test ! -d "$REPO_FOLDER"; then
