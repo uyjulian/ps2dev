400a18971ff810a04f586f1bf779fb1bf36f34aa
uyjulian <uyjulian@gmail.com>
2021-06-11 09:48:45 -0500

    Use my fork of ps2sdk

--- scripts/002-ps2sdk.sh
+++ scripts/002-ps2sdk.sh
@@ -6,11 +6,11 @@
 unset PS2SDKSRC
 
 ## Download the source code.
-REPO_URL="https://github.com/ps2dev/ps2sdk"
+REPO_URL="https://github.com/uyjulian/ps2sdk"
 REPO_FOLDER="ps2sdk"
 
 # Checking if a specific TAG has been selected, it is passed using parameter $1
-[  -z "$1" ] && REPO_REFERENCE="origin/master" || REPO_REFERENCE=$1
+[  -z "$1" ] && REPO_REFERENCE="origin/uyjworking" || REPO_REFERENCE=$1
 echo "Using repo reference $REPO_REFERENCE"
 
 if test ! -d "$REPO_FOLDER"; then
